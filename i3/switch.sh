fb10a93e57081933a69e8f08e9d6611115d8d18e
gn <Gajendra>
2023-04-25 17:41:12 +0530

    added new config for ubuntu 20

---
+++ i3/switch.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+i3-msg "workspace 1, move workspace to output HDMI-0"
+i3-msg "workspace 2, move workspace to output HDMI-0"
+i3-msg "workspace 3, move workspace to output HDMI-0"
+i3-msg "workspace 4, move workspace to output HDMI-0"
---
+++ setup.sh
@@ -0,0 +1,16 @@
+
+# to enable tap to click
+# https://cravencode.com/post/essentials/enable-tap-to-click-in-i3wm/
+# to disable suspend loop 
+# https://help.ubuntu.com/community/RazerBlade#Suspend
+
+sudo apt update
+sudo apt upgrade
+sudo apt install vim htop git tmux feh rofi guake
+sudo apt install python3-pip
+pip install --user bumblebee-status
+# for cpu2 
+pip install psutil
+
+sudo apt install fonts-font-awesome 
+sudo apt install fonts-powerline 
